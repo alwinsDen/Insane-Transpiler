aecace90bc4ffc1225ee0f51f9cee39288fce837
Alwin T <61960848+Alwin8080@users.noreply.github.com>
2021-10-31 06:13:01 +0530

    added basic compiler

--- src/compiler.rs
+++ src/compiler.rs
@@ -1,11 +1,22 @@
-
+use std::fs;
 use std::env;
-use std::path::PathBuf;
+// use std::path::PathBuf;
 use std::path::Path;
 
-pub fn compile_project(project_name: &String)->PathBuf{
+pub fn compile_project(project_name: &String)->String{
     let path_loc = env::current_dir().expect("Getting path failed");
-    let folder_exist : bool =  Path::new(&path_loc).is_dir();
+    let mut comp_project_path = String::new();
+    comp_project_path +=   &path_loc.into_os_string().into_string().unwrap().to_string();
+    comp_project_path += "/";
+    comp_project_path +=      &project_name.to_string();
+    comp_project_path += "/src";
+    let folder_exist : bool =  Path::new(&comp_project_path).is_dir();
+    if folder_exist==true {
+         let contents = fs::read_to_string(format!("{}{}",&comp_project_path,"/index.ic")).expect("Error reading the file.");
+        let line_content:Vec<&str> = contents.split("\n").collect();
+         println!("{:?}",line_content);
+    }
     println!("{:?}",folder_exist);
-    path_loc
+    println!("{:?}",comp_project_path);
+    "Project Compiled".to_string()
 }
